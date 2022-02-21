a88aa1070d33ee4e90e17b498a1112052ca81789
Jona Stevenson <jonatutorials@gmail.com>
2022-02-21 15:45:44 +1300

    Basic Implementation
    Returns whether the item is on special using heliums text function

--- cst.py
+++ cst.py
@@ -1,33 +1,18 @@
-import requests
+from helium import *
+from selenium.webdriver import ChromeOptions
 import time
-from bs4 import BeautifulSoup
 
+options = ChromeOptions()
+#options.add_argument("window-size=1920,1080")
+options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36")
 
-def extractOne(url):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'}
-    r = requests.get(url, headers=headers)
+url='https://shop.countdown.co.nz/shop/productdetails?stockcode=281911&name=mainland-edam-cheese-mild-creamy'
+s_url = 'https://shop.countdown.co.nz/shop/productdetails?stockcode=281739&name=mainland-organic-cheddar-cheese-100-organic-milk'
 
-    soup = BeautifulSoup(r.content, 'html.parser')
-    return soup
+start_chrome(s_url, headless=True, options=options)
 
+if Text('Save $').exists():
+    print('Item on special')
+else: print('Item not on special')
 
-def transformOne(soup):
-
-    try:
-        specialVerifier = soup.find(
-            'span', class_='price price--was ng-star-inserted').text.strip()
-    except:
-        specialVerifier = 'Not on special'
-
-    job = {specialVerifier}
-    print(job)
-
-
-def main():
-    print('Checking if item is on special...')
-    url = 'https://shop.countdown.co.nz/shop/productdetails?stockcode=102323&name=remedy-organic-kombucha-mango-passionfruit'
-    transformOne(extractOne(url))
-
-
-main()
+kill_browser()
