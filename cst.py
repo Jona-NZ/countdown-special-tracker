9a7fa386712e80190a25cbd3fd9c2e2ebeb9e747
Jona Stevenson <jonatutorials@gmail.com>
2022-02-22 18:17:09 +1300

    Refactor

--- cst.py
+++ cst.py
@@ -6,9 +6,10 @@ from dotenv import load_dotenv
 import re
 import os
 import smtplib
+import time
 
 
-def email():
+def email(title, full_price, url):
     load_dotenv()
     gUser = os.environ.get('EMAIL_USER')
     gPass = os.environ.get('EMAIL_PASS')
@@ -33,53 +34,65 @@ def email():
         print('Something went wrong')
 
 
-options = ChromeOptions()
-options.add_argument(
-    "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36")
-options.add_argument("--headless")
-options.add_argument("--no-sandbox")
-options.add_argument("--window-size=1920,1080")
-options.add_argument("--disable-dev-shm-usage")
-options.add_argument('--ignore-certificate-errors')
+def find_elements(driver, stock_code, url):
+    # Get the title
+    try:
+        title = driver.find_element_by_xpath(
+            '//*[@id="product-details"]/div[2]/h1').text
+    except:
+        title = 'Countdown Item '
 
-url = 'https://shop.countdown.co.nz/shop/productdetails?stockcode=281664&name=alpine-cheese-block-tasty'
-stock_code = re.sub("[^0-9]", "", url)
+    # Get and format the price
+    try:
+        uf_price = driver.find_element_by_id(
+            f'product-{stock_code}-price').text
+        f_price = re.sub("[^0-9]", "", uf_price)
 
-driver = webdriver.Chrome(options=options)
-driver.get(url)
+        dollars, cents = f_price[:int(len(f_price)/2)
+                                 ], f_price[int(len(f_price)/2):]
+        full_price = f'${dollars}.{cents}'
+    except:
+        print('Error: Unable to find price')
+        full_price = ''
 
-driver.set_page_load_timeout(15)
+    # Get whether the item is on special
+    try:
+        is_on_special = driver.find_element_by_xpath(
+            '//*[@id="product-details"]/div[2]/div[2]/product-price/div/span[2]').text
+    except:
+        is_on_special = ''
 
-# Get the title
-try:
-    title = driver.find_element_by_xpath(
-        '//*[@id="product-details"]/div[2]/h1').text
-except:
-    title = 'Countdown Item '
+    if is_on_special:
+        print(f'{title} is on special for {full_price}')
+        email(title, full_price, url)
+    else:
+        print(f'{title} is not on special')
 
-# Get and format the price
-try:
-    uf_price = driver.find_element_by_id(f'product-{stock_code}-price').text
-    f_price = re.sub("[^0-9]", "", uf_price)
+    driver.quit()
 
-    dollars, cents = f_price[:int(len(f_price)/2)
-                             ], f_price[int(len(f_price)/2):]
-    full_price = f'${dollars}.{cents}'
-except:
-    print('Error: Unable to find price')
-    full_price = ''
 
-# Get whether the item is on special
-try:
-    is_on_special = driver.find_element_by_xpath(
-        '//*[@id="product-details"]/div[2]/div[2]/product-price/div/span[2]').text
-except:
-    is_on_special = ''
+def main():
+    # Selenium config & run
+    options = ChromeOptions()
+    options.add_argument(
+        "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36")
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument('--ignore-certificate-errors')
 
-if is_on_special:
-    print(f'{title} is on special for {full_price}')
-    email()
-else:
-    print(f'{title} is not on special')
+    url = 'https://shop.countdown.co.nz/shop/productdetails?stockcode=281664&name=alpine-cheese-block-tasty'
+    stock_code = re.sub("[^0-9]", "", url)
 
-driver.quit()
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    driver.set_page_load_timeout(15)
+    time.sleep(3)
+
+    find_elements(driver, stock_code, url)
+
+
+if __name__ == '__main__':
+    main()
